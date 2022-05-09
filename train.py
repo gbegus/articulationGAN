5d25bc803ee683e46a6e1aa6b70eb1a9a5baeb70
Alan Zhou <azhou314@berkeley.edu>
2022-05-09 15:35:47 -0700

    Added tensorboard and argument parsing

--- train.py
+++ train.py
@@ -5,22 +5,23 @@ import torch.nn.functional as F
 from scipy.io.wavfile import read
 from torch.utils.data import Dataset, DataLoader
 from infowavegan import WaveGANGenerator, WaveGANDiscriminator, WaveGANQNetwork
+from torch.utils.tensorboard import SummaryWriter
 
 import os
 import sys
 import glob
 import argparse
+import csv
 from tqdm import tqdm
 
-
 class AudioDataSet:
-    def __init__(self, data_dir):
+    def __init__(self, datadir):
         print("Loading data")
-        dir = os.listdir(data_dir)
+        dir = os.listdir(datadir)
         x = np.zeros((len(dir), 1, 16384))
         i = 0
         for file in tqdm(dir):
-            audio = read(data_dir+file)[1]
+            audio = read(datadir+file)[1]
             if audio.shape[0] < 16384:
                 audio = np.pad(audio, (0, 16384-audio.shape[0]))
             audio = audio[:16384]
@@ -53,17 +54,55 @@ def gradient_penalty(G, D, real, fake, epsilon):
     return penalty
 
 if __name__ == "__main__":
-    # TODO: Add more args
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('data_dir', type=str, help='Training Directory')
+    # Training Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--datadir',
+        type=str,
+        required=True,
+        help='Training Directory'
+    )
+    parser.add_argument(
+        '--logdir',
+        type=str,
+        required=True,
+        help='Log Directory'
+    )
+    parser.add_argument(
+        '--num_categ',
+        type=int,
+        default=1,
+        help='Q-net categories'
+    )
+    parser.add_argument(
+        '--num_epochs',
+        type=int,
+        default=5000,
+        help='Epochs'
+    )
+
+    # Q-net Arguments
+    Q_group = parser.add_mutually_exclusive_group()
+    Q_group.add_argument(
+        '--ciw',
+        action='store_true',
+        help='Trains a ciwgan'
+    )
+    Q_group.add_argument(
+        '--fiw',
+        action='store_true',
+        help='Trains a fiwgan'
+    )
+    args = parser.parse_args()
+    train_Q = args.ciw or args.fiw
 
     # Parameters
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    data_dir = "./training/"
-    log_dir = './checkpoints/'
-    NUM_CATEG = 2
+    datadir = args.datadir
+    logdir = args.logdir
+    NUM_CATEG = args.num_categ
+    NUM_EPOCHS = args.num_epochs
     WAVEGAN_DISC_NUPDATES = 5
-    NUM_EPOCHS = 200
     BATCH_SIZE = 64
     LAMBDA = 10
     LEARNING_RATE = 1e-4
@@ -71,7 +110,7 @@ if __name__ == "__main__":
     BETA2 = 0.9
 
     # Load data
-    dataset = AudioDataSet(data_dir)
+    dataset = AudioDataSet(datadir)
     dataloader = DataLoader(
         dataset,
         BATCH_SIZE,
@@ -83,14 +122,22 @@ if __name__ == "__main__":
     # Load models
     G = WaveGANGenerator().to(device).train()
     D = WaveGANDiscriminator().to(device).train()
-    Q = WaveGANQNetwork(num_categ=NUM_CATEG).to(device).train()
+    if train_Q:
+        Q = WaveGANQNetwork(num_categ=NUM_CATEG).to(device).train()
 
     # Optimizers
     optimizer_G = optim.Adam(G.parameters(), lr=LEARNING_RATE, betas=(BETA1, BETA2))
     optimizer_D = optim.Adam(D.parameters(), lr=LEARNING_RATE, betas=(BETA1, BETA2))
-    optimizer_Q = optim.RMSprop(Q.parameters(), lr=LEARNING_RATE)
-    criterion_Q = torch.nn.BCEWithLogitsLoss()
+    if args.fiw:
+        optimizer_Q = optim.RMSprop(Q.parameters(), lr=LEARNING_RATE)
+        criterion_Q = torch.nn.BCEWithLogitsLoss()
+    elif args.ciw:
+        optimizer_Q = optim.RMSprop(Q.parameters(), lr=LEARNING_RATE)
+        criterion_Q = torch.nn.CrossEntropyLoss()
 
+    # Set Up Writer
+    writer = SummaryWriter(logdir)
+    step=0
     for epoch in range(NUM_EPOCHS):
         print("Epoch {} of {}".format(epoch, NUM_EPOCHS))
         print("-----------------------------------------")
@@ -108,12 +155,14 @@ if __name__ == "__main__":
             penalty = gradient_penalty(G, D, real, fake, epsilon)
 
             D_loss = torch.mean(D(fake) - D(real) + LAMBDA * penalty)
+            writer.add_scalar('Loss/Discriminator', D_loss.detach().item(), i)
             D_loss.backward()
             optimizer_D.step()
 
             if i % WAVEGAN_DISC_NUPDATES == 0:
                 optimizer_G.zero_grad()
-                optimizer_Q.zero_grad()
+                if train_Q:
+                    optimizer_Q.zero_grad()
                 _z = torch.FloatTensor(BATCH_SIZE, 100-NUM_CATEG).uniform_(-1, 1).to(device)
                 c = torch.FloatTensor(BATCH_SIZE, NUM_CATEG).bernoulli_().to(device)
                 z = torch.cat((c, _z), dim=1)
@@ -122,15 +171,21 @@ if __name__ == "__main__":
                 # G Loss
                 G_loss = torch.mean(-D(G_z))
                 G_loss.backward(retain_graph=True)
+                writer.add_scalar('Loss/Generator', G_loss.detach().item(), i)
+
 
                 # Q Loss
-                Q_loss = criterion_Q(Q(G_z), c)
-                Q_loss.backward()
+                if train_Q:
+                    Q_loss = criterion_Q(Q(G_z), c)
+                    Q_loss.backward()
+                    writer.add_scalar('Loss/Q_Network', Q_loss.detach().item(), i)
+                    optimizer_Q.step()
+
 
                 # Update
                 optimizer_G.step()
-                optimizer_Q.step()
+            step+=1
 
-        torch.save(G.state_dict(), f'./checkpoints/epoch{epoch}_G.pt')
-        torch.save(D.state_dict(), f'./checkpoints/epoch{epoch}_D.pt')
-        torch.save(Q.state_dict(), f'./checkpoints/epoch{epoch}_Q.pt')
+        torch.save(G.state_dict(), f'./checkpoints/epoch{epoch}_step{step}_G.pt')
+        torch.save(D.state_dict(), f'./checkpoints/epoch{epoch}_step{step}_D.pt')
+        torch.save(Q.state_dict(), f'./checkpoints/epoch{epoch}_step{step}_Q.pt')
